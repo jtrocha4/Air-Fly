d0db2361f9fcd7d5602459972a3b3c61c0dde11a
Jaime Trocha <jtrocha04@gmail.com>
2022-11-30 16:03:57 -0500

    Componente formulario

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <div className="App">
+
+      <h1>NauticApp</h1>
+
+      <Formulario></Formulario>
       
     </div>
   );
